bf5fd26d1b9556b73ffdd3bc7a4403b2ce2b9685
goldstargreat <goldstargreat@gmail.com>
2026-05-11 09:39:09 +0900

    ViewFlipper 작성 중(plain code)

--- app/src/main/java/kr/ac/kopo/viewflippertest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/viewflippertest/MainActivity.java
@@ -1,10 +1,14 @@
 package kr.ac.kopo.viewflippertest;
 
-import android.os.Bundle;import android.view.View;import android.widget.Button;import android.widget.ViewFlipper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.OnApplyWindowInsetsListener;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -17,11 +21,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                new OnApplyWindowInsetsListener() {
+                    @Override
+                    public WindowInsetsCompat onApplyWindowInsets(View v, WindowInsetsCompat insets) {
+                        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                        return insets;
+                    }
+                }
+        );
 
         btnPrev = findViewById(R.id.btn_prev);
         btnNext = findViewById(R.id.btn_next);
@@ -34,17 +45,12 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Button btnEvent = (Button)v;
-            if (btnEvent == btnPrev)
-            {
+            Button btnEvent = (Button) v;
+            if (btnEvent == btnPrev) {
                 flipper.showPrevious();
-            }
-
-            else
-            {
+            } else {
                 flipper.showNext();
             }
-
         }
     };
 }
